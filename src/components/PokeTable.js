cbb909c10f8d13dad3e50e998c00ae0dc2de2ad6
alejandroolmed59 <00097017@uca.edu.sv>
2021-11-12 15:56:42 -0600

    filter now works properly

--- src/components/PokeTable.js
+++ src/components/PokeTable.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Table, Image, Tag, Carousel } from "antd";
+import { Table, Image, Tag, Carousel, AutoComplete } from "antd";
 import axios from "axios";
 import colorTypes from "./typeColor";
 
+const mapPokemon = (arr) => {
+  return arr.map((pokemon) => {
+    return {
+      value: pokemon.name,
+      pokemonObj: pokemon,
+    };
+  });
+};
+
 const PokeTable = ({ rawPokemons }) => {
   const [dataSource, setDataSource] = useState([]);
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
 
   const fetchEachPoke = async () => {
     let arrPokeDetails = [];
@@ -14,6 +24,7 @@ const PokeTable = ({ rawPokemons }) => {
     }
     console.log(arrPokeDetails);
     setDataSource(arrPokeDetails);
+    setFilteredDataSource(arrPokeDetails);
   };
   useEffect(() => {
     if (rawPokemons.length !== 0) fetchEachPoke();
@@ -48,43 +59,51 @@ const PokeTable = ({ rawPokemons }) => {
           </Tag>
         )),
     },
-    /*    {
-            title: 'Available?',
-            dataIndex:'available',
-            render: (available, record, index)=>(
-                <>
-                    {available?(
-                        <Tag color="green">DISPONIBLE</Tag>
-                    ):(
-                        <Tag color="red">NO DISPONIBLE</Tag>
-                    )}
-                </>
-            ),
-            
-        }*/
   ];
   const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
   };
+
   return (
     <>
       {dataSource.length >= 10 && (
         <Carousel autoplay={true} effect="fade" dotPosition="bottom">
-          {dataSource.slice(0,10).map((poke) => (
-            <div>
-              <Image src={poke.sprites.front_default}/>
+          {dataSource.slice(0, 10).map((poke) => (
+            <div key={poke.key}>
+              <Image src={poke.sprites.front_default} />
               <h1 style={contentStyle}>{poke.name}</h1>
             </div>
           ))}
         </Carousel>
       )}
-      <Table
 
-        dataSource={dataSource}
+      <AutoComplete
+        style={{
+          width: 400,
+        }}
+        options={mapPokemon(dataSource)}
+        placeholder="Pokemon 😼"
+        filterOption={(inputValue, option) =>
+          option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+        }
+        onChange={(value, { pokemonObj }) => {
+          if (value === "") {
+            setFilteredDataSource(dataSource);
+          } else {
+            setFilteredDataSource(
+              dataSource.filter((pokepoke) => {
+                return pokepoke.name.toUpperCase().indexOf(value.toUpperCase())!==-1;
+              })
+            );
+          }
+        }}
+      />
+      <Table
+        dataSource={filteredDataSource}
         columns={columns}
         pagination={{
           defaultPageSize: 25,
